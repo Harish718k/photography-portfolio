cd51fbbba865ce27dbe890dae4f4b446ffc4b2e5
harish718k <harishbca718@gmail.com>
2025-05-10 16:11:44 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var menu = document.querySelector(".menu");
+var navBox = document.querySelector(".nav-box");
+
+menu.addEventListener("click", ()=>{
+    navBox.classList.toggle("hide")
+})
+
+navBox.addEventListener("click", ()=>{
+    navBox.classList.toggle("hide")
+})
