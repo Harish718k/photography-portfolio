b39754777d8a972d27540be68851f25e324446a3
Harish718k <harishbca718@gmail.com>
2025-05-11 18:46:55 +0530

    add feature: slide image function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var menu = document.querySelector(".menu");
 var navBox = document.querySelector(".nav-box");
-
+var num = 1;
 menu.addEventListener("click", ()=>{
     navBox.classList.toggle("hide")
 })
@@ -8,3 +8,27 @@ menu.addEventListener("click", ()=>{
 navBox.addEventListener("click", ()=>{
     navBox.classList.toggle("hide")
 })
+
+function nextImg(event){
+    var image = document.querySelector("#slider1");
+    num = num==3 ? 1 : num+1
+    image.innerHTML = `<img src="img/full-slide/thumb-${num}.jpg" alt="" >`
+}
+
+function previousImg(event){
+    num = num==1 ? 3 : num-1
+    var image = document.querySelector("#slider1");
+    image.innerHTML = `<img src="img/full-slide/thumb-${num}.jpg" alt="" >`
+}
+
+function nImg(event){
+    var image = document.querySelector("#slider2");
+    num = num==4 ? 1 : num+1
+    image.innerHTML = `<img src="img/three-slide/thumb-${num}.jpg" alt="" >`
+}
+
+function pImg(event){
+    num = num==1 ? 4 : num-1
+    var image = document.querySelector("#slider2");
+    image.innerHTML = `<img src="img/three-slide/thumb-${num}.jpg" alt="" >`
+}
